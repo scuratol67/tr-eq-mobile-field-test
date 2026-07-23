b22d62474a432410f1a50356862c393fb70cd17f
scuratol67 <scuratol67@gmail.com>
2026-07-23 15:48:27 -0400

    Fix customer and rep PDF filenames

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v1";
+const CACHE = "tr-eq-field-v2";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

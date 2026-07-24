1c5dffe9ecc1f3951b4080aab21f95cefb390388
scuratol67 <scuratol67@gmail.com>
2026-07-24 16:43:42 -0400

    Use phone-friendly ZIP packages

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v5";
+const CACHE = "tr-eq-field-v6";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

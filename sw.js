522b611e48ba36c67231079ca4216b50c53e5f1d
scuratol67 <scuratol67@gmail.com>
2026-07-24 16:33:51 -0400

    Fix large project export crash

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v4";
+const CACHE = "tr-eq-field-v5";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

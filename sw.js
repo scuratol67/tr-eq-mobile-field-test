d661f6845bf6dc6932a4b706a61aa05f4b8c8fc0
scuratol67 <scuratol67@gmail.com>
2026-07-27 15:30:15 -0400

    Enlarge rep PDF photos and remove image boxes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v7";
+const CACHE = "tr-eq-field-v8";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

9754cf9a56a191f296b997da9651a17041b38bce
scuratol67 <scuratol67@gmail.com>
2026-07-27 16:01:51 -0400

    Place report details between combined photos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v8";
+const CACHE = "tr-eq-field-v9";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

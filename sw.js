0a8b19b5e5aba24f9445cff5de1f5765c55f9abe
scuratol67 <scuratol67@gmail.com>
2026-08-03 13:20:15 -0400

    Connect phone app to Cloudflare live sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v10";
+const CACHE = "tr-eq-field-v11";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

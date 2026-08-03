16413e08a4ae774f4a84619927351862973dde33
scuratol67 <scuratol67@gmail.com>
2026-08-03 11:59:28 -0400

    Fix phone photo uploads and restore live sync
    Compresses cloud upload copies while preserving original full-resolution photos on the phone. Updates the service worker cache to v10.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v9";
+const CACHE = "tr-eq-field-v10";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

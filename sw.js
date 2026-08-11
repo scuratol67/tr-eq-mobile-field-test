08be25682aaf0f65c8f5438e7aed2bd8b05248c0
scuratol67 <scuratol67@gmail.com>
2026-08-11 19:18:47 -0400

    Finish automatic sync and stop optional thumbnail retries
    Upload new field edits automatically, add Admin Sync now, and treat the full-resolution image as complete even when an optional thumbnail cannot be generated.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v18";
+const CACHE = "tr-eq-field-v20";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

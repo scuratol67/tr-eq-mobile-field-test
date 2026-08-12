3175f39ec0f07a8f1920c282fd3d5871d7c187b4
scuratol67 <scuratol67@gmail.com>
2026-08-12 07:06:54 -0400

    Refresh app cache for faster sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v28";
+const CACHE = "tr-eq-field-v29";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

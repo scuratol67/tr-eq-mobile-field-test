fee48dc0fab839f1f9b8de228218893b0b409195
scuratol67 <scuratol67@gmail.com>
2026-08-11 22:32:42 -0400

    Publish master equipment areas and safer admin saves

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v25";
+const CACHE = "tr-eq-field-v26";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));

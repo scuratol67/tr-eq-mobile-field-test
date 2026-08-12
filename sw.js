cc9b7cf29ab69fd90375248fae21cd4db4ab16f2
scuratol67 <scuratol67@gmail.com>
2026-08-11 22:16:27 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,28 @@
-const CACHE = "tr-eq-field-v24";
+const CACHE = "tr-eq-field-v25";
+
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(["./", "./manifest.webmanifest"])).then(() => self.skipWaiting()));
 });
+
 self.addEventListener("activate", event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
+
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
   const url = new URL(event.request.url);
   const sameOrigin = url.origin === self.location.origin;
   const isPhoto = event.request.destination === "image";
+
   if (event.request.mode === "navigate") {
-    event.respondWith(fetch(event.request).then(response => {
+    event.respondWith(caches.match("./").then(cached => cached || fetch(event.request).then(response => {
       const copy = response.clone();
       caches.open(CACHE).then(cache => cache.put("./", copy));
       return response;
-    }).catch(() => caches.match("./")));
+    })));
     return;
   }
+
   if (sameOrigin || isPhoto) {
     event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
       const copy = response.clone();

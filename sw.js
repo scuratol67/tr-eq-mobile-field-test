3487cf3ce3692385cd05ee31420c94f2404a70f9
scuratol67 <scuratol67@gmail.com>
2026-08-13 12:01:46 -0400

    Keep phone projects visible during cloud sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v33";
+const CACHE = "tr-eq-field-v34";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

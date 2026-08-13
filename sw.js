fbe4b0bb194f59ddc9c0f9f6e7a1205913778f41
scuratol67 <scuratol67@gmail.com>
2026-08-13 15:47:07 -0400

    Add exact Essex Dental photo recovery

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v35";
+const CACHE = "tr-eq-field-v36-essex-rescue";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

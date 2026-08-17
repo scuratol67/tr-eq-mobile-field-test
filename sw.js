b8db50f1861dce1416d90814e60651a3728e84d4
scuratol67 <scuratol67@gmail.com>
2026-08-17 16:44:39 -0400

    Add Main Line Endo legacy photo recovery
    Adds a read-only scanner and ZIP export for Main Line Endo photos embedded in older phone app storage. Does not sync, overwrite, or delete project data.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v36-essex-rescue";
+const CACHE = "tr-eq-field-v37-main-line-endo-recovery";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

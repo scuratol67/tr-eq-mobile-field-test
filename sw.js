367cc7d7280ba24f7c203f9d0ffa5497bc767dc1
scuratol67 <scuratol67@gmail.com>
2026-08-18 07:22:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v37-main-line-endo-recovery";
+const CACHE = "tr-eq-field-v39-project-handoff";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

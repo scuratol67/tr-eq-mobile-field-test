74402e05e1d0a202eed6064735f34168440eab15
scuratol67 <scuratol67@gmail.com>
2026-08-18 22:52:20 -0400

    Publish home navigation and unified area workspace

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v53-sort-projects-by-rep";
+const CACHE = "tr-eq-field-v55-home-and-contained-areas";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

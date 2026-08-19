8ba569dd097d3f3e3bf41e142061553cfbb16f81
scuratol67 <scuratol67@gmail.com>
2026-08-18 22:04:28 -0400

    Add home navigation and editable reports

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v49-vertical-folders-project-flags";
+const CACHE = "tr-eq-field-v50-home-report-editing";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

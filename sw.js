62eef6e865823135d458529178a21b3ad945ab4f
scuratol67 <scuratol67@gmail.com>
2026-08-18 22:19:58 -0400

    Make project steps interactive and refresh workspace

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v50-home-report-editing";
+const CACHE = "tr-eq-field-v51-project-workspace";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));

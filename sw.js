c2943d8704ded79dd1ac90fe50d91f972b79d2a6
scuratol67 <scuratol67@gmail.com>
2026-08-18 21:19:05 -0400

    Show redesigned home on existing phones

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tr-eq-field-v46-customer-first-home";
+const CACHE = "tr-eq-field-v47-visible-customer-first-home";
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.add("./manifest.webmanifest")).then(() => self.skipWaiting()));
